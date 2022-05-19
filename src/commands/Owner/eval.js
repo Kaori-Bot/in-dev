f9427a2d7bc38961bb523f81feb6c89874fffee2
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-05-20 00:21:34 +0700

    fixed!

--- src/commands/Owner/eval.js
+++ src/commands/Owner/eval.js
@@ -1,5 +1,4 @@
-const { MessageEmbed } = require("discord.js");
-const { post } = require("node-superfetch");
+const Discord = require("discord.js");
 
 module.exports = {
     name: "eval",
@@ -10,45 +9,24 @@ module.exports = {
     permission: [],
     owner: true,
  execute: async (message, args, client, prefix) => {
-       
-        const embed = new MessageEmbed()
-            .addField("Input", "```js\n" + args.join(" ") + "```");
+        const embed = new Discord.MessageEmbed()
+            .setColor(client.colors.green)
 
         try {
             const code = args.join(" ");
             if (!code) return message.channel.send("Please include the code.");
-            let evaled;
-
-            if (code.includes(`SECRET`) || code.includes(`TOKEN`) || code.includes("process.env")) {
-                evaled = "No, shut up, what will you do it with the token?";
-            } else {
-                evaled = await eval(code);
-            }
+            let evaled = await eval(code);
 
             if (typeof evaled !== "string") evaled = await require("util").inspect(evaled, { depth: 0 });
 
             let output = clean(evaled);
-            if (output.length > 1024) {
-               
-                const { body } = await post("https://hastebin.com/documents").send(output);
-                embed.addField("Output", `https://hastebin.com/${body.key}.js`).setColor(client.colors.toString());
-              
-            } else {
-                embed.addField("Output", "```js\n" + output + "```").setColor(client.colors.toString());
-            }
+            embed.setDescription(`\`\`\`${output}\`\`\``)
 
             message.channel.send({embeds: [embed]});
 
         } catch (error) {
             let err = clean(error);
-            if (err.length > 1024) {
-               
-                const { body } = await post("https://hastebin.com/documents").send(err);
-                embed.addField("Output", `https://hastebin.com/${body.key}.js`).setColor("RED");
-            } else {
-                embed.addField("Output", "```js\n" + err + "```").setColor("RED");
-            }
-
+            embed.setDescription(`\`\`\`\n${err}\`\`\``)
             message.channel.send({embeds: [embed]});
         }
     }
