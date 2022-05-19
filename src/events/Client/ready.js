323ac57ce248e8df73cc7e071dec1419e254ba61
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-05-20 05:45:23 +0700

    delete useles commands

--- src/events/Client/ready.js
+++ src/events/Client/ready.js
@@ -3,10 +3,12 @@ const { prefix } = require("../../config.js");
 module.exports ={
 name: "ready",
 run: async (client) => {
+    await client.application.commands.fetch().then(command => command.delete());
+
     client.registerApplicationCommand('794187901000744991');
     client.manager.init(client.user.id);
  
-    client.logger.log(`${client.user.tag} online!`, "ready");
+    client.logger.log(`${client.user.tag} online!`, 'ready');
     client.user.setStatus('idle');
  }
 }
