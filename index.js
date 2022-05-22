cdf5f00e43c6c7302a87dc1c5013d93ef200dcb8
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-05-22 14:39:28 +0700

    auto post

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ const client = new KaoriClient({
 });
 
 const { AutoPoster } = require('topgg-autoposter');
-if(process.env.TOPGG_TOKEN){
-    const poster = AutoPoster(process.env.TOPGG_TOKEN, client);
-    poster.on('posted', (stats) => {
-        client.logger.log(`Post stats on top.gg/bot/${client.user.id} | ${stats.serverCount} servers`, 'info');
-    });
-}
-else client.logger.log(new RangeError('Topgg-Autoposter: Missing TOPGG_TOKEN!'), 'error');
+const poster = AutoPoster(process.env.TOPGG_TOKEN, client);
+poster.on('posted', (stats) => {
+    client.logger.log(`Post stats on top.gg/bot/${client.user.id} | ${stats.serverCount} servers`, 'info');
+});
 
 process.on('uncaughtException', error => console.error(error))
 process.on('unhandledRejection', error => console.error(error));
