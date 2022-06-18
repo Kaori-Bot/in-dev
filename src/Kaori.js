49d1b2599483bb6ca1e2fda9068d89e299bbe9e2
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-06-18 12:54:41 +0700

    dir

--- src/Kaori.js
+++ src/Kaori.js
@@ -20,25 +20,25 @@ class KaoriBot extends Client {
 			const commandFiles = readdirSync(`./src/commands/${dir}/`).filter(f => f.endsWith('.js'));
 			for (const file of commandFiles) {
 				const command = require(`./commands/${dir}/${file}`);
-				this.commands.categories.push(dir);
 				this.commands.set(command.name, command);
 			}
+			this.commands.categories.push(dir);
 		});
 		this.logger.log('Commands: Loaded...', 'info');
 
-		readdirSync("./src/slashCommands/").forEach((dir) => {
+		readdirSync("./src/slashCommands/").forEach(dir => {
 			const slashCommandFile = readdirSync(`./src/slashCommands/${dir}/`).filter((files) => files.endsWith(".js"));
 			for (const file of slashCommandFile) {
 				const slashCommand = require(`./slashCommands/${dir}/${file}`);
 				if (!slashCommand.name) return console.error(`slashCommandNameError: ${slashCommand.split(".")[0]} application command name is required.`);
 				if (!slashCommand.description) return console.error(`slashCommandDescriptionError: ${slashCommand.split(".")[0]} application command description is required.`);
-				this.slashCommands.categories.push(dir);
 				this.slashCommands.set(slashCommand.name, slashCommand);
 				this.slashCommands._data.push({
 					name: slashCommand.name,
 					description: slashCommand.description,
 					options: slashCommand.options ? slashCommand.options : []
 				});
+				this.slashCommands.categories.push(dir);
 			}
 		});
 		this.logger.log('SlashCommands: Loaded...', 'info');
