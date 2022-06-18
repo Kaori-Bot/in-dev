253b42f930e08add555bae47c06974ff143faff7
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-06-18 17:08:07 +0700

    fixed

--- src/commands/CommandBuilder.js
+++ src/commands/CommandBuilder.js
@@ -8,7 +8,6 @@ class Command {
             inVoiceChannel: data.options ? data.options.inVoiceChannel : false,
             requiredPlaying: data.options ? data.options.requiredPlaying : false,
             sameVoiceChannel: data.options ? data.options.sameVoiceChannel : false,
-            useArguments: data.options ? data.options.useArguments : false
         };
         this.permissions = {
             me: data.permissions ? data.permissions.me : [],
@@ -16,8 +15,9 @@ class Command {
             onlyDj: data.permissions ? data.permissions.onlyDj : false,
             onlyDeveloper: data.permissions ? data.permissions.onlyDeveloper : false,
         };
+        if(data.args) this.args = data.args;
         this.private = data.private || false;
-        this.constructor.execute = data.execute ? typeof data.execute == 'function' ? data.execute : () => {throw new TypeError(`${this.constructor.name}.execute`, 'Type is not function!')} : () => {};
+        this.execute = data.execute ? typeof data.execute == 'function' ? data.execute : () => {throw new TypeError(`${this.constructor.name}.execute`, 'Type is not function!')} : () => {};
     }
     get category() {
         const { readdirSync } = require('fs');
@@ -29,9 +29,6 @@ class Command {
             if (commands.includes(this.name+'.js')) commandCategory = directory;
         });
         return commandCategory;
-    }
-    execute(...args) {
-        return this.constructor.execute.bind(this)(...args);
     }
 };
 
