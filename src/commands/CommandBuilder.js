2e0e4d20507cffa8b1365dee5318682e43361b79
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-06-18 12:45:41 +0700

    fixed

--- src/commands/CommandBuilder.js
+++ src/commands/CommandBuilder.js
@@ -15,15 +15,15 @@ class Command {
             user: data.permissions ? data.permissions.user : []
         };
         this.private = data.private || false;
-        this.constructor.execute = typeof data.execute == 'function' ? data.execute : () => {throw new TypeError(`${this.constructor.name}.execute`, 'Type is not function!')};
+        this.constructor.execute = data.execute ? typeof data.execute == 'function' ? data.execute : () => {throw new TypeError(`${this.constructor.name}.execute`, 'Type is not function!')} : () => {};
     }
     get category() {
         const { readdirSync } = require('fs');
-        const categories = readdirSync('./commands').filter(x => !x.includes('.'));
+        const categories = readdirSync('./src/commands').filter(x => !x.includes('.'));
 
         let commandCategory = '';
         categories.forEach(directory => {
-            const commands = readdirSync(`./commands/${directory}`);
+            const commands = readdirSync(`./src/commands/${directory}`);
             if (commands.includes(this.name+'.js')) commandCategory = directory;
         });
         return commandCategory;
